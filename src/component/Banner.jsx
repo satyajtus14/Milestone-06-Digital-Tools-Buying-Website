5da39e373176f6cf1847c07c65178a2d0568098d
“satyajitus14” <sjbarua14@gmail.com>
2026-03-29 21:50:12 -0400

    banner done

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -5,6 +5,7 @@ import { CgEditBlackPoint } from "react-icons/cg";
 
 const Banner = () => {
     return (
+      <div className='space-y-8'>
         <div className="relative min-h-162.5 flex items-center overflow-hidden">
         <div className=" mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 items-center relative z-10">
           {/* Left Content */}
@@ -61,6 +62,37 @@ const Banner = () => {
           </div>
         </div>
       </div>
+      
+      <div className='h-50 w-full text-white bg-gradient-to-r from-[#4f39f6] via-[#9514fa] from-[#4f39f6] flex justify-evenly items-center'>
+        <div className='flex flex-col items-center gap-1'>
+        <h1 className='font-bold text-6xl'>50k+</h1>
+        <p>Active Users</p>
+        </div> 
+     
+        <div className="w-px h-16 bg-white/30" />
+      
+       <div className='flex flex-col items-center gap-1'>
+       <h1 className='font-bold text-6xl'>200+</h1>
+       <p>Premium Tools</p>
+       </div>
+      
+       <div className="w-px h-16 bg-white/30" />
+        
+      <div className='flex flex-col items-center gap-1'>
+      <h1 className='font-bold text-6xl'>4.9</h1>
+      <p>Rating</p>
+      </div>
+      
+
+    </div>
+    <div className='text-center'>
+      <h1 className='text-6xl font-bold'>Premium Digital Tools</h1> <br />
+      <p className='text-[#627382]'>
+      Choose from our curated collection of premium digital products designed <br />
+      to boost your productivity and creativity.
+      </p>
+    </div>
+    </div>
     );
 };
 
